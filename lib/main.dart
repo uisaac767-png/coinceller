fc94c7d6b4b6b4fbec9c5bf631e482081bae27bc
Isaac <uisaac767@gmail.com>
2026-02-10 20:35:01 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'screens/auth_screen.dart';
 import 'theme/bybit_theme.dart';
+import 'test_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  
   const MyApp({super.key});
-
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -18,7 +20,7 @@ class MyApp extends StatelessWidget {
         scaffoldBackgroundColor: Colors.black, // ✅ fixed
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const AuthScreen(), // ✅ start from AuthScreen
+      home: const TestScreen(), // ✅ start from AuthScreen
     );
   }
 }
