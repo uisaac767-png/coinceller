abe3e46825c74f6135027472f97c37f7bbb84e41
Isaac <uisaac767@gmail.com>
2026-02-10 20:29:25 +0100

    Create test_screen.dart

---
+++ lib/test_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TestScreen extends StatelessWidget {
+  const TestScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          "Celler App Loaded Successfully",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
