da02d5032e57c795bc1d1f81a9294074da4bb42b
Isaac <uisaac767@gmail.com>
2026-02-11 13:18:14 +0100

    Add splash screen with logo and navigation
    Implement a splash screen that displays a logo and navigates to the login screen after 3 seconds.

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => const LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          "assets/images/logo.png",
+          width: 120,
+        ),
+      ),
+    );
+  }
+}
