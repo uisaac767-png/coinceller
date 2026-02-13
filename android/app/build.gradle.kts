2fe47a11de05e89e2b64f1502f1b9410bf2b807b
Isaac Uche <gabriellouis346@gmail.com>
2026-02-13 09:40:33 +0100

    Fixed android structure and MainActivity crash

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,7 +20,10 @@ android {
     }
 
     defaultConfig {
+        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.coin_celler"
+        // You can update the following values to match your application needs.
+        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -29,10 +32,8 @@ android {
 
     buildTypes {
         release {
-            // Prevent minify/shrink to avoid instant crashes
-            isMinifyEnabled = false
-            isShrinkResources = false
-            // Use debug signing for now (safe for testing)
+            // TODO: Add your own signing config for the release build.
+            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
